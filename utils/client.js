fb504be5b250c67cb77b2008bde7a5c54645784c
Imran <md.imransyam1956@gmail.com>
2023-05-23 00:31:54 +0600

    needs to change dataset by manually adding production to it because env variables are not working properly

--- utils/client.js
+++ utils/client.js
@@ -6,7 +6,7 @@ import createImageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
   projectId: '58rpdysb',
-  dataset: process.env.NEXT_PUBLIC_DATASET,
+  dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
