d298990f43e0b9e949a2512166e902078c0e3871
Imran <md.imransyam1956@gmail.com>
2023-05-23 19:57:02 +0600

    return to previous commit

--- utils/fetchQueries.ts
+++ utils/fetchQueries.ts
@@ -2,28 +2,28 @@ import { PageInfo, Project, Skill, Social } from "@/typings"
 
 
 export const fetchSocials = async() => {
-    const res = await fetch(`https://intern-portfolio.sanity.studio/api/social`, {cache: 'force-cache'})
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/social`, {cache: 'force-cache'})
     const data = await res.json()
     const socials: Social[] = data.socials
     return socials
 }
 
 export const fetchSkills = async() => {
-    const res = await fetch(`https://intern-portfolio.sanity.studio/api/skill`, {cache: 'force-cache'})
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/skill`, {cache: 'force-cache'})
     const data = await res.json()
     const skills: Skill[] = data.skills
     return skills
 }
 
 export const fetchProjects = async() => {
-    const res = await fetch(`https://intern-portfolio.sanity.studio/api/projects`, {cache: 'force-cache'})
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projects`, {cache: 'force-cache'})
     const data = await res.json()
     const projects: Project[] = data.projects
     return projects
 }
 
 export const fetchPageInfo = async() => {
-    const res = await fetch(`https://intern-portfolio.sanity.studio/api/pageInfo`, {cache: 'force-cache'})
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pageInfo`, {cache: 'force-cache'})
     const data = await res.json()
     const pageInfo: PageInfo = data.pageInfo
     return pageInfo
