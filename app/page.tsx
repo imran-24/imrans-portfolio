a5b54d1352e3e181f427bf455ee0ec6b205f0c59
Imran <md.imransyam1956@gmail.com>
2023-09-01 18:56:38 +0600

    updated the domain

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,16 @@ const inter = Inter({ subsets: ['latin'] })
 export default async function Home() {
 
   // static data 
-  // const pageInfo: PageInfo = await fetchPageInfo()
-  // const socials: Social[] = await fetchSocials()
-  // const skills: Skill[] = await fetchSkills()
-  // const projects: Project[] = await fetchProjects()
+  const pageInfo: PageInfo = await fetchPageInfo()
+  const socials: Social[] = await fetchSocials()
+  const skills: Skill[] = await fetchSkills()
+  const projects: Project[] = await fetchProjects()
 
 
   return (
     <div className='w-full h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-rose-500/50'>
       
-      {/* <Header 
+      <Header 
       socials={socials} 
       />
       <section className='snap-center' id='hero'>
@@ -35,9 +35,9 @@ export default async function Home() {
       </section>
       <section className='snap-center' id='about'>
         <About pageInfo={pageInfo} />
-      </section> */}
+      </section>
       {/* <Experience /> */}
-      {/* <section className='snap-center' id='skills'>
+      <section className='snap-center' id='skills'>
         <Skills skills={skills}/>
       </section>
       <section className='snap-center' id='projects'>
@@ -45,7 +45,7 @@ export default async function Home() {
       </section>
       <section className='snap-center' id='contact'>
         <Contact />
-      </section> */}
+      </section>
 
       
         <footer className='sticky bottom-5 w-full cursor-pointer z-50'>
