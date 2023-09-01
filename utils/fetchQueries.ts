f7807b5ad8adcfe8e6d230540acaaa66a2aa78f9
Imran <md.imransyam1956@gmail.com>
2023-09-01 19:18:34 +0600

    formated url

--- utils/fetchQueries.ts
+++ utils/fetchQueries.ts
@@ -2,28 +2,28 @@ import { PageInfo, Project, Skill, Social } from "@/typings"
 
 
 export const fetchSocials = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/social`)
+    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/social`)
     const data = await res.json()
     const socials: Social[] = data.socials
     return socials
 }
 
 export const fetchSkills = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/skill`)
+    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/skill`)
     const data = await res.json()
     const skills: Skill[] = data.skills
     return skills
 }
 
 export const fetchProjects = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/projects`)
+    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/projects`)
     const data = await res.json()
     const projects: Project[] = data.projects
     return projects
 }
 
 export const fetchPageInfo = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/pageInfo`)
+    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/pageInfo`)
     const data = await res.json()
     const pageInfo: PageInfo = data.pageInfo
     return pageInfo
