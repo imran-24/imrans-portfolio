e7ed1f9ac1c06cb121ef1d37941ba8f79e9b8542
Imran <md.imransyam1956@gmail.com>
2023-09-01 17:50:24 +0600

    updated api

--- utils/fetchQueries.ts
+++ utils/fetchQueries.ts
@@ -2,28 +2,28 @@ import { PageInfo, Project, Skill, Social } from "@/typings"
 
 
 export const fetchSocials = async() => {
-    const res = await fetch(`/api/social`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/social`)
     const data = await res.json()
     const socials: Social[] = data.socials
     return socials
 }
 
 export const fetchSkills = async() => {
-    const res = await fetch(`/api/skill`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/skill`)
     const data = await res.json()
     const skills: Skill[] = data.skills
     return skills
 }
 
 export const fetchProjects = async() => {
-    const res = await fetch(`/api/projects`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projects`)
     const data = await res.json()
     const projects: Project[] = data.projects
     return projects
 }
 
 export const fetchPageInfo = async() => {
-    const res = await fetch(`/api/pageInfo`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pageInfo`)
     const data = await res.json()
     const pageInfo: PageInfo = data.pageInfo
     return pageInfo
